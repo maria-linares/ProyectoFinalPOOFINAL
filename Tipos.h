e411d2f6e5995d237b282f245dfd6121be093be6
Cesar Bracamonte <cesar.bracamonte@utec.edu.pe>
2019-06-24 18:49:46 -0500

    Primera modificación pruebas

--- Tipos.h
+++ Tipos.h
@@ -2,7 +2,14 @@
 // Created by utec on 21/06/19.
 //
 
-#ifndef GAME_TIPOS_H
-#define GAME_TIPOS_H
+#ifndef AGREGACION_TIPOS_H
+#define AGREGACION_TIPOS_H
 
-#endif //GAME_TIPOS_H
+#include <string>
+using namespace std;
+// Definiendo alias
+using TipoEntero = int;
+using TipoCaracter = char;
+using TipoString = string;
+
+#endif //AGREGACION_TIPOS_H
