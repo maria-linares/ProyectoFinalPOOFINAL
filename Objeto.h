6c98ab6bf377745289727d2c6e534debf30233cb
Maria Jose Linares <majolive@Majos-MacBook.local>
2019-06-25 10:30:04 -0500

    Subir objeto

--- Objeto.h
+++ Objeto.h
@@ -7,8 +7,25 @@
 
 
 class Objeto {
-
+private:
+    string        nombre;
+    TipoCaracter  color;
+    TipoEntero    posX;
+    TipoEntero    posY;
+public:
+    Objeto();
+    Objeto(const TipoString& nombre, TipoCaracter color,
+           TipoEntero posX, TipoEntero posY);
+    virtual ~Objeto();
+    void setNombre(const TipoString& nombre);
+    string     getNombre();
+    TipoEntero getPosX();
+    TipoEntero getPosY();
+    char getColor();
+    void moverse(TipoEntero x, TipoEntero y);
+    string mostrarPosicion();
 };
+
 
 
 #endif //GAME_OBJETO_H
