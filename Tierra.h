25b99981efa444218faaa7660d7ba0af1b46dd62
Raul Suarez <raul.suarez@utec.edu.pe>
2019-06-24 19:17:23 -0500

    Primera subida de Tierr

--- Tierra.h
+++ Tierra.h
@@ -2,13 +2,26 @@
 // Created by utec on 21/06/19.
 //
 
-#ifndef GAME_TIERRA_H
-#define GAME_TIERRA_H
-
-
-class Tierra {
-
+#include "Tipos.h"
+using namespace std;
+class Objeto {
+private:
+    string nombre;
+    TipoCaracter color;
+    TipoEntero posX;
+    TipoEntero posY;
+public:
+    Objeto();
+    Objeto(const TipoString& nombre, TipoCaracter color,
+           TipoEntero posX, TipoEntero posY);
+    virtual ~Objeto();
+    void setNombre(const TipoString& nombre);
+    string getNombre();
+    TipoEntero getPosX();
+    TipoEntero getPosY();
+    char getColor();
+    void moverse(TipoEntero x, TipoEntero y);
+    string mostrarPosicion();
 };
+#endif //AGREGACION_OBJETO_H
 
-
-#endif //GAME_TIERRA_H
